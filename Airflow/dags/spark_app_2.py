28e0ab7534e84f65f4e97074dbd46f71df854b1d
perceptiveperceval <34310793+perceptiveperceval@users.noreply.github.com>
2023-01-18 22:02:31 +0700

    Update spark_app_2.py

--- Airflow/dags/spark_app_2.py
+++ Airflow/dags/spark_app_2.py
@@ -1,9 +1,18 @@
 import pyspark
+import os
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
+
+path = os.getcwd()
+name = "42.5.1.jar"
+
+for root, dirs, files in os.walk(path):
+        if name in files:
+                app_path = (os.path.join(root, name))
+                break
 spark = SparkSession \
         .builder \
-        .config("spark.jars", "/tmp/postgresql-42.5.1.jar") \
+        .config("spark.jars", app_path) \
         .master("local").appName("PySpark_Postgres_test").getOrCreate()
 
 df = spark.read \
